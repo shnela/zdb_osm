9c88da50166cc9ca73f289a597c9d35ab283144d
Jakub Kuszneruk <jakub.kuszneruk@adpilot.pl>
2015-10-16 20:03:57 +0200

    [INIT] db connection from python script

--- import_osm_data.py
+++ import_osm_data.py
@@ -1,6 +1,18 @@
-# WROK IN PROGRESS
+# WORK IN PROGRESS
 
+import json
 import psycopg2
 
+
+def get_connection_string():
+    with open('db_config.json', 'r') as f:
+        config = f.read()
+    config_dict = json.loads(config)
+    conn_string = "host='%s' dbname='%s' user='%s' password='%s'"\
+                  % (config_dict['host'], config_dict['db_name'], config_dict['user'], config_dict['password'])
+    return conn_string
+
 if __name__ == "__main__":
-  pass
+    conn_string = get_connection_string()
+
+    conn = psycopg2.connect(conn_string)
